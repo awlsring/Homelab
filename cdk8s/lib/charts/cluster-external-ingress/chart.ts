95ac6609a2ba2f1a2752a0fc93c7d66b4fe44dd8
awlsring <mattcanemail@gmail.com>
2023-01-09 18:22:52 -0800

    refactor: migrate to new jellyfin server

--- cdk8s/lib/charts/cluster-external-ingress/chart.ts
+++ cdk8s/lib/charts/cluster-external-ingress/chart.ts
@@ -89,7 +89,7 @@ export class ClusterExternalIngressChart extends Chart {
   makeJellyfin(props: ClusterExternalIngressChartProps) {
     const jellyfin = new Service(this, "jellyfin", {
       type: ServiceType.EXTERNAL_NAME,
-      externalName: "10.0.100.11",
+      externalName: "10.0.10.148",
       ports: [
         {
           name: "jellyfin",
