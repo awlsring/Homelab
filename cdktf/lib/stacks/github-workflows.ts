f4aad2634f6cd65c45bc4c8d539dd8f645cdbcdb
awlsring <mattcanemail@gmail.com>
2023-01-23 23:13:11 -0800

    tmp: workflow stack

---
+++ cdktf/lib/stacks/github-workflows.ts
@@ -0,0 +1,33 @@
+import { Construct } from "constructs";
+import { HomelabStack, HomelabStackProps } from "../constructs/homelab-stack";
+import { GithubProvider } from '@cdktf/provider-github/lib/provider';
+import { Job, Workflow } from "@awlsring/cdktf-github-actions";
+
+export interface GithubWorkflowsStackProps extends HomelabStackProps {
+  repoName: string;
+  githubToken: string;
+}
+
+export class GithubWorkflowsStack extends HomelabStack {
+  constructor(scope: Construct, name: string, props: GithubWorkflowsStackProps) {
+    super(scope, name, props);
+
+    new GithubProvider(this, "provider", {
+      token: props.githubToken,
+    });
+
+    let echoJob = new Job(this, 'build-job', {
+      steps: [
+        {
+          name: 'echo hello',
+          run: 'echo "Hello"',
+        },
+      ],
+    });
+
+    const proxmoxTest = new Workflow(this, "proxmox-test", {
+      repoName: props.repoName,
+    });
+
+  }
+}
