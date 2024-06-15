8680d7d5eb8e708919dc60d697a288afc89408a4
Matthew Rawlings <contact@matthewrawlings.com>
2024-06-15 10:56:31 -0700

    refactor(immich): raise resource limits

--- packages/cdk8s-constructs/src/constructs/apps/immich.ts
+++ packages/cdk8s-constructs/src/constructs/apps/immich.ts
@@ -481,11 +481,11 @@ export class Immich extends Construct {
       resources: {
         cpu: {
           request: Cpu.millis(200),
-          limit: Cpu.millis(1000),
+          limit: Cpu.millis(4000),
         },
         memory: {
           request: Size.mebibytes(256),
-          limit: Size.gibibytes(1),
+          limit: Size.gibibytes(8),
         },
       },
       liveness: Probe.fromHttpGet("/server-info/ping", {
